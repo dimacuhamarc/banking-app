00a85faf8f99541f66081c73c8f1fed38d9fe328
Marc Dimacuha <dimacuha.marc@gmail.com>
2023-09-28 20:38:13 +0800

    adds routing, dashboard, and login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+
+const Login = () => {
+  return(
+    <div>
+      <h1>Login</h1>
+      <Link to='dashboard'> Login </Link>
+    </div>
+  );
+};
+
+export default Login;
