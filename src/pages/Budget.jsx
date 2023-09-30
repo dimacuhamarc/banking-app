8679384ea37f51f3bc7a29ec6f2626ec6608fd74
paulenepal <paulenepallanx@gmail.com>
2023-10-01 01:17:06 +0800

    Added Calendar & Expenses components for the Budget.jsx. Updated index.html to include font-awesome script for icons. Updated _variable.scss to include new colors and gradient combi.

--- src/pages/Budget.jsx
+++ src/pages/Budget.jsx
@@ -1,10 +1,46 @@
+import React from "react";
 import DashboardNav from "../components/Dashboard/DashboardNav/DashboardNav";
+import { Card, CardStyles } from "../components/Dashboard/DashboardCards/Card";
+import Expense from "../components/BudgetApp/Expenses/Expenses";
+import "../styles/Dashboard.scss";
+import Calendar from "react-calendar";
+import "../styles/Budget.scss";
 
 const Budget = () => {
   return (
     <div>
       <DashboardNav />
-      <h1>Budget App</h1>
+      <div className="dashboard">
+        <h1>Budget Tracker</h1>
+        <div className="main-budget-container">
+          <div className="dashboard-cards">
+            <Card
+              data={`PHP ${Intl.NumberFormat("en-PH", 2).format(10000.43)}`}
+              title={"Total Bank Balance"}
+              style={CardStyles.cash}
+              icon={<i class="fa-solid fa-money-bill"></i>}
+            />
+          </div>
+
+          <div className="budget-content">
+            <div className="first-container">
+              <div className="graph">*Graph Here*</div>
+
+              <div className="calendar">
+                Calendar
+                <Calendar></Calendar>
+              </div>
+            </div>
+
+            <div className="second-container">
+              <div className="expense">
+                <Expense></Expense>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
